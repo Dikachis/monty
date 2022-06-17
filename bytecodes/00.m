9a12dd7aea0b92da0b853ee8a4da04bcd9f7311a
duffigoogle <calebdduff@gmail.com>
2022-06-17 00:33:55 +0000

    fix and update bytecodes

--- bytecodes/00.m
+++ bytecodes/00.m
@@ -1,4 +1,4 @@
-push 1$
-push 2$
-push 3$
-pall$
+push 1
+push 2
+push 3
+pall
--- bytecodes/000.m
+++ bytecodes/000.m
@@ -1,9 +1,9 @@
-push 0$
-push 1$
-push 2$
-  push 3$
-                   pall    $
-push 4$
-    push 5    $
-      push    6        $
-pall$
+push 0
+push 1
+push 2
+  push 3
+                   pall    
+push 4
+    push 5    
+      push    6        
+pall
